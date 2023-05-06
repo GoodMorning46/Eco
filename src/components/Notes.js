199bbe3adfe4f713dab96d89eaf3345a21ac9d40
Good_Morning <benji@macbook-pro-de-benjamin.home>
2023-05-06 12:30:50 +0200

    Plus d'options d'édition

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from "react";
 import ReactQuill from "react-quill";
+import Quill from 'quill';
 import "react-quill/dist/quill.snow.css"; // importez le CSS de Quill
 import "./css/Notes.css";
+
+const toolbarOptions = [
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  [{ 'align': [] }],
+  ['bold', 'italic', 'underline', 'strike'], // Les options de base sont déjà là
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }], // Listes
+  [{ 'color': [] }, { 'background': [] }], // Couleurs
+  ['link', 'image'], // Liens, images et vidéos
+];
+
+const quill = new Quill('#editor', {
+  modules: {
+    toolbar: toolbarOptions,
+  },
+  theme: 'snow',
+});
 
 export default function App() {
   const localNotes = localStorage.getItem("notes");
@@ -20,7 +37,7 @@ export default function App() {
 
   return (
     <div className="notes-container">
-      <ReactQuill value={notes} onChange={handleChange} />
+      <ReactQuill value={notes} onChange={handleChange} modules={{ toolbar: toolbarOptions }} />
     </div>
   );
 }
